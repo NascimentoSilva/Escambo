abe90c729ff61c5cc202240e0c231bc4e0ee5176
NascimentoSilva <jns2@cin.ufpe.br>
2016-11-16 04:30:53 -0300

    add upload img

--- teste/urls.py
+++ teste/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.post, name="link"),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_full, name="link2"),
+    url(r'^$', views.post, name="home"),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_full, name="detalhe"),
 ]
-#if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
